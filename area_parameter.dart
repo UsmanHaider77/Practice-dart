0e0d184a19ed836c0fc370d20b5be94a63a1433b
UsmanHaider77 <musmanhaider77@gmail.com>
2024-01-26 14:23:13 -0800

    Area & Parameter

---
+++ area_parameter.dart
@@ -0,0 +1,16 @@
+   import 'dart:math';
+
+void main(){
+
+   cricleArea(5);
+   cricleArea(4);
+
+   }
+
+   cricleArea(double r){
+    double carea = pi * r * r;
+    double cparameter = 2 * pi * r;
+    print("Cricle Area is = $carea");
+    print("Cricle Parameter is = $cparameter");
+
+   }
